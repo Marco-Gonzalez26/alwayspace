d8cd3be21d9e75fe9370da40d6aff1c33a86f9b8
Marco Gonzalez <magp2606@gmail.com>
2022-12-15 11:18:03 -0500

    Added vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 import vercel from "@astrojs/vercel/serverless";
 
+
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
